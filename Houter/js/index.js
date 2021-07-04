a8ef6c235c434ddc26a3d17bb1948f6079563d8e
Umarkhon <umarkhonakhmedov2000@gmail.com>
2021-07-05 04:50:32 +0500

    Adding review section for mobile

--- Houter/js/index.js
+++ Houter/js/index.js
@@ -16,3 +16,40 @@ if(navClose){
   })
 }
 
+// Slide Show
+const first = document.getElementById("review__menu-first"),
+      second = document.getElementById("review__menu-second"),
+      third = document.getElementById("review__menu-third"),
+      review = document.getElementById("review__list");
+
+if(first){
+  first.addEventListener('click', () => {
+    review.classList.add("show__first"),
+    review.classList.remove("show__third"),
+    review.classList.remove("show__second"),
+    first.style.backgroundColor = "#3C4563",
+    second.style.backgroundColor = "#E0E3EB";
+    third.style.backgroundColor = "#E0E3EB";
+  })
+}
+if(second){
+  second.addEventListener('click', () => {
+    review.classList.remove("show__first"),
+    review.classList.remove("show__third"),
+    review.classList.add("show__second");
+    first.style.backgroundColor = "#E0E3EB",
+    second.style.backgroundColor = "#3C4563";
+    third.style.backgroundColor = "#E0E3EB";
+  })
+}
+if(third){
+  third.addEventListener('click', () => {
+    review.classList.remove("show__first"),
+    review.classList.add("show__third"),
+    review.classList.remove("show__second");
+    first.style.backgroundColor = "#E0E3EB",
+    second.style.backgroundColor = "#E0E3EB";
+    third.style.backgroundColor = "#3C4563";
+  })
+}
+
