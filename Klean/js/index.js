a8104978b089348744e1c3de0e8aef3ccc9907b4
Umarkhon <umarkhonakhmedov2000@gmail.com>
2021-07-13 16:03:38 +0500

    Adding navbar section for desktop size

---
+++ Klean/js/index.js
@@ -0,0 +1,17 @@
+const list = document.getElementById('nav__main-list'),
+      links = document.getElementById('nav__links'),
+      menu = document.getElementById('nav__menu'),
+      close = document.getElementById('nav__main-close');
+
+
+function open(){
+  list.classList.add('show__menu');
+  links.classList.add('show__links');
+}
+menu.addEventListener('click', open);
+
+function closed(){
+  list.classList.remove('show__menu');
+  links.classList.remove('show__links');
+}
+close.addEventListener('click', closed)
