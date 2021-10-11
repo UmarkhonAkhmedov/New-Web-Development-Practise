29b0fd1f3c19d2a0f54dd5a8e8c42d2ec3430817
UmarkhonAkhmedov <umarkhonakhmedov2000@gmail.com>
2021-10-11 21:31:11 +0500

    Doing some javascript for changing the image

--- Hotel Booking Freebie/js/script.js
+++ Hotel Booking Freebie/js/script.js
@@ -18,3 +18,19 @@ if(closeBtn){
     navHambur.classList.remove('remove__hambur');
   })
 }
+
+
+// Changing the selection option image
+const nathan = document.querySelector(".nathan");
+const smith = document.querySelector(".smith");
+const adam = document.querySelector(".adam");
+const image = document.querySelector(".header__info--select");
+
+if(nathan){
+  nathan.addEventListener('option', () => {
+    var img = document.createElement('img');
+    img.src = "../img/top__option-men.svg";
+    document.getElementById('header__info--select').appendChild(img);
+    console.log("clicked")
+  })
+}
