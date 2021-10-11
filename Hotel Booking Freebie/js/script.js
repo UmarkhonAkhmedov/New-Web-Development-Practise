df659ee2f96238bd5531aee13050c4e73fc04df8
UmarkhonAkhmedov <umarkhonakhmedov2000@gmail.com>
2021-10-11 05:43:22 +0500

    creating hamburger menu for navigation bar

--- Hotel Booking Freebie/js/script.js
+++ Hotel Booking Freebie/js/script.js
@@ -1,1 +1,20 @@
-c
+const navList = document.querySelector(".nav");
+const closeBtn = document.querySelector(".nav__list--close");
+const navHambur = document.querySelector(".nav__hambur");
+
+
+
+if(navHambur){
+  navHambur.addEventListener('click', () => {
+    navList.classList.add('show__bar');
+    navHambur.classList.add('remove__hambur');
+
+  })
+}
+
+if(closeBtn){
+  closeBtn.addEventListener('click', () =>{
+    navList.classList.remove('show__bar');
+    navHambur.classList.remove('remove__hambur');
+  })
+}
